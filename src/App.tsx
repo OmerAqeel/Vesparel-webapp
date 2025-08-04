53a36bf1bc0ff368bcb8b8f8b05c4e1c2212976f
OmerAqeel <omrraqeel@gmail.com>
2025-08-04 20:55:34 +0100

    added Hero Image, made the webapp more responsive for different devices, added hamburger for smaller screens in navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import { Button } from "@/components/ui/button"
 import { Navbar } from "./compo/Navbar"
+import { Message } from "./compo/Message"
+import { HomeMainImage } from "./compo/HomeMainImage"
 
 function App() {
   return (
     <div className="">
+      <Message />
       <Navbar />
+      <HomeMainImage />
+      {/* Add more components or content as needed */}
     </div>
   )
 }
